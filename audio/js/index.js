2edb4a5e9edfe063fb3554148f0f018841e164a7
kimSerVer <kimserver@111percent.net>
2021-12-24 16:58:31 +0900

    fix: 미디어 녹음 베이스 조사 완료 파일을 저장할 때, format은 ogg 로 해야한다

--- audio/js/index.js
+++ audio/js/index.js
@@ -1,26 +1,16 @@
 async function getMedia(constraints) {
   let stream = null;
-  
 
   try {
-    const audioContext = new AudioContext();
     stream = await navigator.mediaDevices.getUserMedia(constraints);
-    const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream)
-    const destinationNode = audioContext.createMediaStreamDestination()
-    console.log(audioContext.destination)
-    
-    
-    console.log("helloworld")    
     const handles = await window.showSaveFilePicker();
-    const createWritable = await handles.createWritable();
+    const createWritable = await handles.createWritable();   
   
     const rec = new MediaRecorder(stream);
-    const chunks = [];
 
-
-    rec.ondataavailable = (e) => {
+    rec.ondataavailable = (e) => {      
+      e.data.type = 'audio/ogg codecs=opus'
       createWritable.write(e.data)
-      console.log(e);
     };
     rec.onstop = () => createWritable.close()
 
@@ -28,12 +18,13 @@ async function getMedia(constraints) {
 
     setTimeout(() => {
       rec.stop();
-    }, 5000)
+    }, 10 * 1000)
 
   } catch(err) {
     /* 오류 처리 */
     console.log(err)
   }
+
 }
 
 getMedia({ audio: true })
